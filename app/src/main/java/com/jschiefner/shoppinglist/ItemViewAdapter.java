f2968a868626e2276c770df4cfd04066fb09383e
Jonas Schiefner <jonas.schiefner@zweitag.de>
2020-04-14 18:38:24 +0200

    add simple items recycler view

--- app/src/main/java/com/jschiefner/shoppinglist/ItemViewAdapter.java
+++ app/src/main/java/com/jschiefner/shoppinglist/ItemViewAdapter.java
@@ -1,39 +1,79 @@
 package com.jschiefner.shoppinglist;
 
+import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ItemViewAdapter extends RecyclerView.Adapter<ItemViewAdapter.ItemViewHolder> {
+    private List<Item> items = new ArrayList<>();
 
     public static class ItemViewHolder extends RecyclerView.ViewHolder {
         public EditText editText;
+        public int position;
 
         public ItemViewHolder(@NonNull View itemView) {
             super(itemView);
             editText = itemView.findViewById(R.id.recycler_edit_text);
+            this.editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                    Log.i("CUSTOM", textView.getText().toString());
+                    InputMethodManager imm = (InputMethodManager) textView.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(textView.getWindowToken(), 0);
+
+                    return true;
+                }
+            });
+            this.editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View view, boolean hasFocus) {
+                    if (hasFocus) return;
+
+                    EditText editText = (EditText) view;
+                    Log.i("CUSTOM", editText.getText().toString());
+                }
+            });
         }
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+        notifyDataSetChanged();
     }
 
     @NonNull
     @Override
     public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_recycler_view, parent, false);
-        ItemViewHolder iwh = new ItemViewHolder(view);
-        return iwh;
+        return new ItemViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
-
+        Item currentItem = items.get(position);
+        holder.editText.setText(currentItem.name);
+        holder.position = position;
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.size();
     }
 }
--- app/src/main/java/com/jschiefner/shoppinglist/ShoppingFragment.java
+++ app/src/main/java/com/jschiefner/shoppinglist/ShoppingFragment.java
@@ -5,21 +5,44 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ShoppingFragment extends Fragment {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
+    private ItemViewModel itemViewModel;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.shopping_fragment, container, false);
+        View rootView = inflater.inflate(R.layout.shopping_fragment, container, false);
+
+        RecyclerView recyclerView = rootView.findViewById(R.id.items_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setHasFixedSize(true);
+        final ItemViewAdapter adapter = new ItemViewAdapter();
+
+        recyclerView.setAdapter(adapter);
+
+        // Setup Trip List
+        itemViewModel = ViewModelProviders.of(this, new ItemViewModelFactory(getActivity().getApplication())).get(ItemViewModel.class);
+        itemViewModel.getItems().observe(this, new Observer<List<Item>>() {
+            @Override
+            public void onChanged(List<Item> items) {
+                adapter.setItems(items);
+            }
+        });
+
+
+        return rootView;
 
 
     }
